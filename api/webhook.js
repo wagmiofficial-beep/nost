2401f583c7f94c32b6c07d9652ec117b005aec8f
wagmiofficial-beep <xgmwagmi.official@gmail.com>
2025-11-11 14:53:34 +0700

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 // api/webhook.js
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   const frame = {
     version: "vNext",
     image: "https://nost.vercel.app/og.png",
@@ -10,10 +10,11 @@ module.exports = (req, res) => {
         action: "link",
         target: "https://nost.vercel.app"
       }
-    ]
+    ],
+    postUrl: "https://nost.vercel.app/api/webhook"
   };
 
   res.setHeader("Content-Type", "application/json");
   res.status(200).json(frame);
-};
+}
 
